70d4932794c58f75540809f05cdf3fa00f04066d
sony <sagarshukla100@gmail.com>
2016-03-14 11:21:58 +0530

    First Commit

---
+++ RestFulExp/src/main/java/com/sagar/rest/DtdcCourierResource.java
@@ -0,0 +1,25 @@
+package com.sagar.rest;
+
+import javax.ws.rs.GET;
+import javax.ws.rs.Path;
+import javax.ws.rs.Produces;
+import javax.ws.rs.QueryParam;
+
+@Path("/dtdcCourier")
+public class DtdcCourierResource {
+
+	@GET
+	// to tell this is
+	@Produces("Text/plain")
+	// client will send request so the format it will specify
+	public String trackingConsinment(
+			@QueryParam(value = "courierNo") String courierNo,
+			@QueryParam("name") String name) {
+		return "hii your courier no. is " + courierNo + " and name is " + name;
+
+	}
+
+	public DtdcCourierResource() {
+		System.out.println("from Contructor");
+	}
+}
